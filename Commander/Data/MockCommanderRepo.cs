b54c4f1f9f057dd38f5a4f1054a13712f13dde6b
JaroslawKlima <jaroslaw.klima@aprosoft.pl>
2021-08-20 20:20:38 +0200

    Add project files.

---
+++ Commander/Data/MockCommanderRepo.cs
@@ -0,0 +1,37 @@
+﻿using Commander.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public void CreateCommand(Command cmd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Command> GetAllCommands()
+        {
+            var commands = new List<Command> 
+            {
+                new Command { Id = 0, HowTo = "test1", Line = "test1", Platform = "test1" },
+                new Command { Id = 1, HowTo = "test1", Line = "test1", Platform = "test1" },
+                new Command { Id = 2, HowTo = "test1", Line = "test1", Platform = "test1" }
+            };
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command { Id = 0, HowTo = "test1", Line = "test1", Platform = "test1" };
+        }
+
+        public bool SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
